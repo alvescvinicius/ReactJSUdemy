37f892c1a78ecf35fcf8bdb074091a08655fd912
alvescvinicius <alvescvinicius@gmail.com>
2020-11-11 22:41:55 -0300

    Renderizando primeiro ocmponente

---
+++ meusite/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App(){
+    return (
+
+      <div>
+          <h1>Olá Mundo !</h1>
+      </div>  
+
+    );
+}
+
+// Exporta para poder ser acesso em outra lugar
+export default App;
--- meusite/src/Social.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-function Social(){
-    return <h1>Social</h1>
-}
-
-// Exporta para poder ser acesso em outra lugar
-export default Social;
