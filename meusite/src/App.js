2045cf2684e8d0849ad322a1ca9344df12de53e1
alvescvinicius <alvescvinicius@gmail.com>
2020-11-11 22:56:35 -0300

    Usando props stateless

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 // Criando stateless component (só uma linha não precisa de {})
 // const Bemvindo = () => <h2>Bem-vindo(a)</h2>
-const Bemvindo = () => {
+const Bemvindo = (props) => {
     return (
         <div>
-            <h2>Bem-vindo(a)</h2>
+            <h2>Bem-vindo(a) {props.nome}</h2>
+            <h3>Tenho {props.anos} anos</h3>
         </div> 
     );
 }
@@ -15,7 +16,8 @@ function App(){
 
       <div>
             Olá Mundo !
-            <Bemvindo/>
+            <Bemvindo nome="Lucas" anos="24"/>
+            <Bemvindo nome="Amanda" anos="19"/>
             <h1>Curso React</h1>
       </div>  
 
