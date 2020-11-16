ec909aac2580d07e287dd1b8b0586ee3cd42800c
alvescvinicius <alvescvinicius@gmail.com>
2020-11-15 22:04:55 -0300

    ciclos de vida dos components

--- meusite/src/App.js
+++ meusite/src/App.js
@@ -3,51 +3,28 @@ import React, { Component } from 'react';
 class App extends Component {
 
   constructor(props) {
-    // Super usa as props do pai no caso render(){}
     super(props);
-    // States referentes ao object App no caso
     this.state = {
-      nome: 'Matheus',
-      contador: 0
+      hora: '00:00:00'
     };
-
-    // Faz funcao aumentar atualizar o valor
-    this.aumentar = this.aumentar.bind(this);
-    this.diminuir = this.diminuir.bind(this);
-
   }
 
-  aumentar() {
-
-    let state = this.state;
-    state.contador += 1;
-    this.state.nome = 'Jose';
-    this.setState(state);
+  // Esse é executado uma unica vez quando o component é montado
+  componentDidMount(){
+    //atualiza hora de 1 em 1 segundo
+    setInterval(()=>{
+      this.setState({hora: new Date().toLocaleString()});
+    }, 1000);
   }
 
-  diminuir() {
-
-    let state = this.state;
-
-    if (state.contador === 0) {
-      alert('Opa chegou a zero !');
-      return;
-    }
-
-    state.contador -= 1;
-    this.setState(state);
+  componentDidUpdate(){
+    console.log('Atualizou !!!');
   }
 
   render() {
     return (
       <div>
-        <h1>Contador</h1>
-        {this.state.nome}
-        <h3>
-          <button onClick={this.diminuir}>-</button>
-          {this.state.contador}
-          <button onClick={this.aumentar}>+</button>
-        </h3>
+        <h1>Meu projeto {this.state.hora}</h1>
       </div>
     );
   }
