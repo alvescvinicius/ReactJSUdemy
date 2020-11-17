3aadaa47a2f5a2bcf99374e05d59fc16069528d7
alvescvinicius <alvescvinicius@gmail.com>
2020-11-17 00:00:41 -0300

    Fibase configs 1

--- nox-studio-web/src/App.js
+++ nox-studio-web/src/App.js
@@ -3,17 +3,24 @@ import React, { Component } from 'react';
 import { BrowserRouter,Switch,Route } from 'react-router-dom';
 import Home from './component/Home';
 import Header from './component/Header';
-import './global.css'
+import './global.css';
 
 class App extends Component {
+
+  state = {
+    firebaseInitialized: false
+  };
+
   render(){
-    return (
+    return this.state.firebaseInitialized !== false ? (
       <BrowserRouter>
       <Header/>
         <Switch>
           <Route exact path='/' component={Home}/>
         </Switch>
       </BrowserRouter>
+    ) : (
+      <h1>Carregando...</h1>
     );
   }
 }
